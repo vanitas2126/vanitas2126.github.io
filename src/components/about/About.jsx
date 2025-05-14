cd4ab1def00dd4d435836426d1b4d9a35d44e8bf
vanitas2126 <daudzulfikar7@gmail.com>
2025-05-14 18:32:27 +0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	renamed:    src/assets/Example-Cv.pdf -> src/assets/DaudZulfikar-Cv.pdf
    	modified:   src/components/about/About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/aboutBW.jpeg";
-import CV from "../../assets/Example-Cv.pdf";
+import CV from "../../assets/DaudZulfikar-Cv.pdf";
 import Info from "./Info";
 
 const About = () => {
